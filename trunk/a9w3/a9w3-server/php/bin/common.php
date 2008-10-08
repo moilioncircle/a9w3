3b9e7d232127d40f56d6bc3d46aab7362fe04bb7
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2008-10-08 09:29:20 +0000

    *** empty log message ***

--- trunk/a9w3/a9w3-server/php/bin/common.php
+++ trunk/a9w3/a9w3-server/php/bin/common.php
@@ -2,16 +2,17 @@
 define("PATH_ROOT","../../../");
 define("DEFAULT_PASS","a9w3_1s_g00d");
 define("SKEY_IMGSN","IMGSN_");
-define("SKEY_ADMIN","ADMIN_");
-define("SKEY_CPASS","CPASS_");
+define("SKEY_UTIME","UTIME_");
+define("SKEY_UMODE","UMODE_");
+define("UMODE_ADMIN","ADMIN");
+define("UMODE_WRITER","WRITER");
+
 define("RKEY_UNKOWN","warn.unknown");
 define("RKEY_SUCCESS","info.success");
 define("RKEY_WRIMGSN","warn.imgsn.wrong");
 define("RKEY_ACCDENY","warn.deny");
 define("RKEY_SETPASS","warn.pass.change");
 define("RKEY_SMPPASS","info.pass.simple");
-
-
 
 function checkRequestUID(){
     if(empty($_REQUEST["UID"])) return false;
@@ -23,11 +24,22 @@ function checkRequestUID(){
 
 function checkAdminPermit(){
     @session_start();
-    if(empty($_SESSION[SKEY_IMGSN.$_REQUEST["UID"]])){
+    if(empty($_SESSION[SKEY_ADMIN.$_REQUEST["UID"]])){
         echo RKEY_ACCDENY;
         exit;
     }
-    if(time() - $_SESSION[SKEY_IMGSN.$_REQUEST["UID"]] >= 5*60){
+    if(time() - $_SESSION[SKEY_ADMIN.$_REQUEST["UID"]] >= 5*60){
+        echo RKEY_ACCDENY;
+        exit;
+    }
+}
+function checkWriterPermit(){
+    @session_start();
+    if(empty($_SESSION[SKEY_WRITER.$_REQUEST["UID"]])){
+        echo RKEY_ACCDENY;
+        exit;
+    }
+    if(time() - $_SESSION[SKEY_WRITER.$_REQUEST["UID"]] >= 5*60){
         echo RKEY_ACCDENY;
         exit;
     }
