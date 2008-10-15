958ab659790adb3cf26b3b25d508884060cc98f8
Shi Rongjiu <trydofor@gmail.com>
2008-10-15 14:59:09 +0000

    *** empty log message ***

--- a9w3/a9w3-server/php/bin/common-indexer.php
+++ a9w3/a9w3-server/php/bin/common-indexer.php
@@ -7,7 +7,55 @@ function appendIndex($tp,$uid,$pid,$lbl){
     if(!isValidType($tp)){
         return false;
     }
+    $idir = PATH_ROOT.'a9w3-auhome/'.$uid.'/indexer/'.$tp; //address
+    // label
+    if(!empty($lbl)){
+        $maxseq = 0;
+        $valkey = array();
+        $idxfl = $idir.'/label/item.htm';
+        foreach(file($idxfl) as $line){
+            $kv = explode("=", $line);
+            if(count($kv) !=2) continue;
+            $valkey[trim($kv[1])] = $kv[0];
+            if(intval($kv[0])>$maxseq){
+                $maxseq = intval($kv[0]);
+            }
+        }
+        foreach(preg_split('/[\s,=]+/', $lbl) as $part){
+            
+            if (array_key_exists($part, $valkey)){
+                $lbto = $valkey[$part];
+            }else{
+                $maxseq ++;
+                $lbto = sprintf('%03s',$maxseq);
+                if(!is_dir($idir.'/label/'.$lbto)
+                &&mkdir($idir.'/label/'.$lbto)
+                &&!writeFile($idxfl,"\n$lbto=$part",'a+')){ //label/item.htm
+                    return false;
+                }
+            }
+            if(!writeFile($idir.'/label/'.$lbto.'/item.htm',"\n$pid",'a+')){
+                return false;
+            }
+        }
+    }
+    // month
+    $quto = date('Y').'$'.intval((date('m')-1)/4+1); //2008$3
+    $itemdir = $idir.'/month/'.$quto; //month/2008$3/
+    if(!is_dir($itemdir)
+    && mkdir($itemdir) 
+    && !writeFile($idir.'/month/item.htm',"\n".$quto,'a+')){
+        return false; //month/item.htm
+    }
+    if(!writeFile($itemdir.'/item.htm',"\n".$pid,'a+')){
+        return false;//month/2008$3/item.htm
+    }
+    // total
+    if(!writeFile($idir.'/total/item.htm',"\n".$pid,'a+')){
+        return false;//month/2008$3/item.htm
+    }
     
+    return true;
 }
 function deleteIndex($tp,$uid,$pid,$lbl){
     if(!isValidType($tp)){
@@ -20,4 +68,8 @@ function isValidType($tp){
     || $tp === IDX_ARTICLE
     || $tp === IDX_GALLERY);
 }
+
+/*if(appendIndex(IDX_ADDRESS,'a9admin','20081015212121','ÎÒµÄ, ²âÊÔ abc')){
+    echo 'ok';
+}*/
 ?>
