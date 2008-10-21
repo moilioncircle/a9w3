fb25c3765cd836b4d8701c4410ebdae8d9787c7f
Shi Rongjiu <trydofor@gmail.com>
2008-10-21 17:14:51 +0000

    *** empty log message ***

--- a9w3/a9w3-server/php/bin/links-delete.php
+++ a9w3/a9w3-server/php/bin/links-delete.php
@@ -10,8 +10,18 @@ if(empty($_REQUEST['PID'])){
 }
 
 // delete file
+if(!deleteFile(PATH_ROOT.'a9w3-auhome/'.$_REQUEST['UID'].'/address/'.$_REQUEST['PID'].'.htm';)){
+    echo RKEY_UNKOWN;
+    exit;
+}
 
 // remove index
+if(!removeIndexFromTotal(IDX_ADDRESS,$_REQUEST['UID'],$_REQUEST['PID'])
+|| !removeIndexFromMonth(IDX_ADDRESS,$_REQUEST['UID'],$_REQUEST['PID'])
+|| !removeIndexFromLabel(IDX_ADDRESS,$_REQUEST['UID'],$_REQUEST['PID'])){
+    echo RKEY_UNKOWN;
+    exit;
+}
 
 echo RKEY_SUCCESS;
 ?>
