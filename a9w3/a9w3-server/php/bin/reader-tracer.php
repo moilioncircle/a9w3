7dc97b833a9ac927653d659119d6dea3bd60a79f
Shi Rongjiu <trydofor@gmail.com>
2008-10-30 13:33:11 +0000

    *** empty log message ***

--- a9w3/a9w3-server/php/bin/reader-tracer.php
+++ a9w3/a9w3-server/php/bin/reader-tracer.php
@@ -1,18 +1,22 @@
 <?php
-switch($_REQUEST['type']){
-    case 'paper':
-        break;
-    case 'album':
-        break;
-    default:
-        exit;
-}
-
-// checkuid
 require_once('common.php');
 checkRequestUID();
 
-// checkpid
+// check type and pid
+if(empty($_REQUEST['PID'])) exit;
 
-
+require_once('common-readlog.php');
+if($_REQUEST['type'] === 'paper'){
+    if(!preg_match('/^[0-9]{4}\/[0-9]{10}+$/', $_REQUEST['PID'])) exit;
+    if(!is_dir(PATH_ROOT.'a9w3-auhome/'.$_REQUEST['UID'].'/article/'.$_REQUEST['PID'])) exit;
+    $tp = RDL_ARTICLE;
+}else if ($_REQUEST['type'] === 'album'){
+    if(!preg_match('/^[0-9]{4}\/[0-9]{13}+$/', $_REQUEST['PID'])) exit;
+    if(!is_file(PATH_ROOT.'a9w3-auhome/'.$_REQUEST['UID'].'/gallery/info/'.$_REQUEST['PID'].'.htm')) exit;
+    $tp = RDL_GALLERY;
+}else{
+    exit;
+}
+// write log
+traceReader($tp,$_REQUEST['UID'],$_REQUEST['PID']);
 ?>
