4f6a0a5613eb7f66cace13dec3e80fb9bebeacc5
Shi Rongjiu <trydofor@gmail.com>
2008-10-31 12:22:40 +0000

    *** empty log message ***

--- a9w3/a9w3-server/php/bin/album-commit.php
+++ a9w3/a9w3-server/php/bin/album-commit.php
@@ -3,4 +3,11 @@ require_once('common.php');
 checkRequestUID();
 checkUmodePermit(UMODE_WRITER);
 
+// PID|CODE (UID,[PID],[LABEL],BRIEF,[FILE?PID!=NULL])
+if(empty($_REQUEST['BRIEF'])
+||(empty($_REQUEST['PID']) && empty($_REQUEST['FILE']))){
+    echo RKEY_ACCDENY;
+    exit;
+}
+
 ?>
