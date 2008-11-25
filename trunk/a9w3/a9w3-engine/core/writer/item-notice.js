3845fcc5cab6e700387b588705f79d18240ac14e
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2008-11-25 08:04:19 +0000

    *** empty log message ***

--- trunk/a9w3/a9w3-engine/core/writer/item-notice.js
+++ trunk/a9w3/a9w3-engine/core/writer/item-notice.js
@@ -95,7 +95,11 @@ function onViewData(){
 
 function onViewPaper(){
     var win = window.open('about:blank','newwindow', 'height=600, width=600, top=0, left=0, toolbar=no,scrollbars=yes, resizable=yes, menubar=no,location=no, status=no');
-    win.document.write("<base href='"+docPath+"'>");
+    if(isNewPaper()){
+    	win.document.write("<base href='"+parent.W3CNF.USERHOME+"indexer/article/label/'>");
+    }else{
+    	win.document.write("<base href='"+docPath+"'>");
+    }
     win.document.write(__EDIOR__.getText());
     win.document.close();
 }
