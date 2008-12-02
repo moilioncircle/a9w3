295f730332b6b29afcaf093001d46da23d87d805
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2008-12-02 09:27:08 +0000

    *** empty log message ***

--- trunk/a9w3/a9w3-engine/core/reader/list-status.js
+++ trunk/a9w3/a9w3-engine/core/reader/list-status.js
@@ -1,51 +1,56 @@
 var lineHeight=16;
+var splitbg = "#EFEFEF";
+var blankbg = "#FFFFFF";
 
 function initReader(){
-	var html = "";
-	html+="Pager: "+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/article/0000.htm")+" read";
-	html+="&nbsp;&nbsp;&nbsp;";
-	html+="Album: "+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/gallery/0000.htm")+" read";
-	html+="</br></br>";
-    document.getElementById("READER").innerHTML=html;
+	var html = [];
+	html.push("PagerHits: "+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/article/0000.htm"));
+	html.push("&nbsp;&nbsp;");
+	html.push("AlbumHits: "+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/gallery/0000.htm"));
+	html.push("</br>");
+    document.getElementById("READER").innerHTML=html.join("");
 }
 
 function initStatus(){
     var keys = ["admin","address","article","gallery","board","notice"];
-    var html= "<table border='0' cellspacing='0' cellpadding='0' width='100%'>";
-        html+="<tr height='"+lineHeight+"'><td width='80'>Channel</td><td width='40'>Cnt</td><td>Last Date</td></tr>";
+    var html = [];
+    html.push("<table border='0' cellspacing='1' cellpadding='2' width='100%'>");
+    html.push("<tr height='"+lineHeight+"'><td width='80'>Channel</td><td width='40'>Cnt</td><td>Last Date</td></tr>");
 	for(var i=0;i<keys.length;i++){
-		html+="<tr height='"+lineHeight+"'>";
-		html+="<td>"+keys[i]+"</td>";
-        html+="<td>"+parent.W3GUI.STAT.getValue(keys[i]+".count")+"</td>";
-		html+="<td>"+parent.W3GUI.STAT.getValue(keys[i]+".mtime").substr(0,10)+"</td>";
-		html+="</tr>";
+		html.push("<tr height='"+lineHeight+"' style='background-color:"+blankbg+"'>");
+		html.push("<td>"+keys[i]+"</td>");
+        html.push("<td>"+parent.W3GUI.STAT.getValue(keys[i]+".count")+"</td>");
+		html.push("<td>"+parent.W3GUI.STAT.getValue(keys[i]+".mtime").substr(0,10)+"</td>");
+		html.push("</tr>");
 	}
-	html+="</table><br/>";
-	document.getElementById("STATUS").innerHTML=html;
+	html.push("</table><br/>");
+	document.getElementById("STATUS").innerHTML=html.join("");
 }
 
 function initAction(){
-	var html = "";
+	var html = [];
 	var acts = parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/write/top30.htm");
 	if(acts != null){
-		var html= "<table border='0' cellspacing='0' cellpadding='0' width='100%'>";
-		    html+="<tr height='"+lineHeight+"'><td width='20'>Id</td><td width='70'>Date</td><td>Name</td></tr>";
+		html.push("<table border='0' cellspacing='1' cellpadding='2' width='100%'>");
+		html.push("<tr height='"+lineHeight+"'><td width='20'>Id</td><td width='70'>Date</td><td>Name</td></tr>");
 		var lines = acts.split(/[\r\n]+/).reverse();
 		var cnt = lines.length>15?15:lines.length;
+        var j=1;
 		for(var i=0;i<cnt;i++){
 			var parts = lines[i].split("|");
 			if(parts.length != 4) continue;
 			var dt = parts[0];
 			var at = parts[1];
-			html+="<tr height='"+lineHeight+"'>";
-			html+="<td>"+(i+1)+"</td>";
-			html+="<td>"+dt.substr(0,8)+"</td>";
-			html+="<td>"+at+"</td>";
-			html+="</tr>";
+            html.push("<tr height='"+lineHeight+"' style='background-color:"+(j%5 ==0?splitbg:blankbg)+"'>");
+			html.push("<td>"+j+"</td>");
+			html.push("<td>"+dt.substr(0,8)+"</td>");
+			html.push("<td>"+at+"</td>");
+			html.push("</tr>");
+            j++;
 		}
-		html+="</table><br/>";
+		html.push("</table><br/>");
 	}
-	document.getElementById("ACTION").innerHTML=html;
+	document.getElementById("ACTION").innerHTML=html.join("");
 }
 initReader();
 initStatus();
