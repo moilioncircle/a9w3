06dccfd471b6c1dd837bc0fd8950ad86345804c4
Shi Rongjiu <trydofor@gmail.com>
2009-01-23 02:05:08 +0000

    *** empty log message ***

--- a9w3/a9w3-engine/core/reader/list-status.js
+++ a9w3/a9w3-engine/core/reader/list-status.js
@@ -3,11 +3,11 @@ var splitbg = "#EFEFEF";
 var blankbg = "#FFFFFF";
 
 function initReader(){
-	var html = [];
-	html.push("Pager:"+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/article/0000.htm"));
-	html.push("&nbsp;&nbsp;");
-	html.push("Album:"+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/gallery/0000.htm"));
-	html.push("</br>");
+    var html = [];
+    html.push("Pager:"+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/article/0000.htm"));
+    html.push("&nbsp;&nbsp;");
+    html.push("Album:"+parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/read/gallery/0000.htm"));
+    html.push("</br>");
     document.getElementById("READER").innerHTML=html.join("");
 }
 
@@ -16,43 +16,45 @@ function initStatus(){
     var html = [];
     html.push("<table border='0' cellspacing='1' cellpadding='2' width='100%'>");
     html.push("<tr height='"+lineHeight+"'><td width='55'>Channel</td><td width='35'>Cnt</td><td>Last Date</td></tr>");
-	for(var i=0;i<keys.length;i++){
-		html.push("<tr height='"+lineHeight+"' style='background-color:"+blankbg+"'>");
-		html.push("<td>"+keys[i]+"</td>");
+    for(var i=0;i<keys.length;i++){
+        html.push("<tr height='"+lineHeight+"' style='background-color:"+blankbg+"'>");
+        html.push("<td>"+keys[i]+"</td>");
         html.push("<td>"+parent.W3GUI.STAT.getValue(keys[i]+".count")+"</td>");
-		html.push("<td>"+parent.W3GUI.STAT.getValue(keys[i]+".mtime").substr(0,10)+"</td>");
-		html.push("</tr>");
-	}
-	html.push("</table>");
-	document.getElementById("STATUS").innerHTML=html.join("");
+        html.push("<td>"+parent.W3GUI.STAT.getValue(keys[i]+".mtime").substr(0,10)+"</td>");
+        html.push("</tr>");
+    }
+    html.push("</table>");
+    document.getElementById("STATUS").innerHTML=html.join("");
 }
 
 function initAction(){
-	var maxl =10;
-	var html = [];
-	var acts = parent.A9Loader.syncLoadText(parent.W3CNF.USERHOME+"helpers/status/write/top10.htm");
-	if(acts != null){
-		html.push("<table border='0' cellspacing='1' cellpadding='2' width='100%' style='overflow:hidden;'>");
-		html.push("<tr height='"+lineHeight+"'><td width='20'>Id</td><td width='70'>Date</td><td>Name</td></tr>");
-		var lines = acts.split(/[\r\n]+/).reverse();
-		var cnt = lines.length>maxl?maxl:lines.length;
-        var j=1;
-		for(var i=0;i<cnt;i++){
-			var parts = lines[i].split("|");
-			if(parts.length != 4) continue;
-			var dt = parts[0];
-			var at = parts[1];
-            html.push("<tr height='"+lineHeight+"' style='background-color:"+(j%5 ==0?splitbg:blankbg)+"'>");
-			html.push("<td>"+j+"</td>");
-			html.push("<td>"+dt.substr(0,8)+"</td>");
-			html.push("<td>"+at+"</td>");
-			html.push("</tr>");
-            j++;
-		}
-		html.push("</table><br/>");
-	}
-	document.getElementById("ACTION").innerHTML=html.join("");
+    var maxl =10;
+    parent.W3GUI.asyncLoadText(function(acts){
+        var html = [];
+        if(acts != null){
+            html.push("<table border='0' cellspacing='1' cellpadding='2' width='100%' style='overflow:hidden;'>");
+            html.push("<tr height='"+lineHeight+"'><td width='20'>Id</td><td width='70'>Date</td><td>Name</td></tr>");
+            var lines = acts.split(/[\r\n]+/).reverse();
+            var cnt = lines.length>maxl?maxl:lines.length;
+            var j=1;
+            for(var i=0;i<cnt;i++){
+                var parts = lines[i].split("|");
+                if(parts.length != 4) continue;
+                var dt = parts[0];
+                var at = parts[1];
+                html.push("<tr height='"+lineHeight+"' style='background-color:"+(j%5 ==0?splitbg:blankbg)+"'>");
+                html.push("<td>"+j+"</td>");
+                html.push("<td>"+dt.substr(0,8)+"</td>");
+                html.push("<td>"+at+"</td>");
+                html.push("</tr>");
+                j++;
+            }
+            html.push("</table><br/>");
+        }
+        document.getElementById("ACTION").innerHTML=html.join("");
+    },parent.W3CNF.USERHOME+"helpers/status/write/top10.htm");
 }
+//
 initReader();
 initStatus();
 initAction();
