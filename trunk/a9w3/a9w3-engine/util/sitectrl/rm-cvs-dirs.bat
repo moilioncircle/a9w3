8e4c8f58ce5eee33f05ba328a77b1e7975f6ebd1
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2009-02-18 07:04:15 +0000

    *** empty log message ***

--- trunk/a9w3/a9w3-engine/util/sitectrl/rm-cvs-dirs.bat
+++ trunk/a9w3/a9w3-engine/util/sitectrl/rm-cvs-dirs.bat
@@ -1,7 +1,5 @@
-@echo On
-@Rem rm CVS dirs
-
-@for /r . %%a in (.) do @if exist "%%a\CVS\" rmdir /q /s "%%a\CVS\"
-
-@echo Mission Completed.
-@pause
+@echo off
+Rem rm CVS dirs
+echo CTRL + C to break, others to continue.
+pause
+for /r . %%a in (.) do @if exist "%%a\CVS\" rmdir /q /s "%%a\CVS\"
