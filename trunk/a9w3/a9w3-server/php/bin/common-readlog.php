52bf601239886c2beb7cf3871f6d3cb40fca737a
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2009-02-18 07:15:52 +0000

    *** empty log message ***

--- trunk/a9w3/a9w3-server/php/bin/common-readlog.php
+++ trunk/a9w3/a9w3-server/php/bin/common-readlog.php
@@ -9,38 +9,75 @@ function traceReader($tp,$uid,$pid){
     && $tp !== RDL_GALLERY){
         return false;
     }
-    $fnm = PATH_ROOT.'a9w3-auhome/'.$uid.'/helpers/status/read/'.$tp.'/'.$pid;
-    $bdr = dirname($fnm);
+    
+    $ymdhis = getUserDate('YmdHis',$uid);
+    
+    /**** read hit/log ****/
+    $rfnm = PATH_ROOT.'a9w3-auhome/'.$uid.'/helpers/status/read/'.$tp.'/'.$pid;
+    $bdr = dirname($rfnm);
     if(!is_dir($bdr) && !mkdir($bdr)){ // mkdir if not exist
         return false;
     }
-    //
+    // cnt
     $ttl = PATH_ROOT.'a9w3-auhome/'.$uid.'/helpers/status/read/'.$tp.'/0000.htm';
-    $cnt = 1;
-    if(is_file($ttl)){
-    	$cnt = intval(trim(file_get_contents($ttl)))+1;
-    }
-    if(!writeFile($ttl,$cnt,'w')){
+    if(!increaseHit($ttl)){
         return false;
     }
-    
+    // hit
+    $hnm = $rfnm.'-hit.htm';
+    if(!increaseHit($hnm)){
+        return false;
+    }
     // log
-    $lnm = $fnm.'-log.htm';
-    $lgt = getUserDate('YmdHis',$uid).'|'.$_SERVER["REMOTE_ADDR"]."\n";
+    $lnm = $rfnm.'-log.htm';
+    $lgt = $ymdhis.'|'.$_SERVER["REMOTE_ADDR"]."\n";
     if(!writeFile($lnm,$lgt,'a+')){
         return false;
     }
     
-    // hit
-    $hnm = $fnm.'-hit.htm';
-    $hit = 1;
-    if(is_file($hnm)){
-        $hit = intval(trim(file_get_contents($hnm)))+1;
+    /**** stat hit/log ****/
+    $sfnm = PATH_ROOT.'a9w3-auhome/'.$uid.'/helpers/status/stat/'.$tp;
+    $yyyy = substr($ymdhis,0,4);
+    $mm   = substr($ymdhis,4,2);
+    $dd   = substr($ymdhis,6,2);
+    
+    $sdfy = $sfnm.'/date/'.$yyyy;
+    $sdfm = $sdfy.'/'.$mm;
+    $sdf  = $sdfm.'/'.$dd;
+    $smfy = $sfnm.'/month/'.$yyyy;
+    $smf  = $smfy.'/'.$mm;
+    $syf  = $sfnm.'/year/'.$yyyy;
+    
+    if((!is_dir($sdfy) && !mkdir($sdfy))
+    || (!is_dir($sdfm) && !mkdir($sdfm))
+    || (!is_dir($smfy) && !mkdir($smfy))){
+        echo RKEY_UNKOWN;
+        exit;
     }
-    if(!writeFile($hnm,$hit,'w')){
+    
+    if(!increaseHit($sdf.'-hit.htm')
+    || !increaseHit($smf.'-hit.htm')
+    || !increaseHit($syf.'-hit.htm')){
         return false;
     }
     
+        // log
+    $lnm = $sdf.'-log.htm';
+    $lgt = $tp.'|'.$pid.'|'.$ymdhis.'|'.$_SERVER["REMOTE_ADDR"]."\n";
+    if(!writeFile($lnm,$lgt,'a+')){
+        return false;
+    }
+    return true;
+}
+
+function increaseHit($fln){
+    $cnt = 1;
+    if(is_file($fln)){
+        $cnt = intval(trim(file_get_contents($fln)))+1;
+    }
+    if(!writeFile($fln,$cnt,'w')){
+        return false;
+    }
     return true;
 }
 ?>
