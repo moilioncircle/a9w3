eb4e9d32e36006ce65fe66813b6b55779d122e57
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2009-02-20 06:11:38 +0000

    *** empty log message ***

--- trunk/a9w3/a9w3-engine/core/reader/item-rdstat.js
+++ trunk/a9w3/a9w3-engine/core/reader/item-rdstat.js
@@ -4,13 +4,44 @@ var blankbg = "#FFFFFF";
 var datexbg = "#DDDDEF";
 
 var isStop = true;
+var isHead = false;
 var typeHr = '';
 var dateFr = new Date();
 var dateTo = new Date();
 
+var HEAD_POOL = {};
+
+function getHead(tp,pid){
+    if(!isHead) return pid;
+    var pk = tp+pid;
+    if(HEAD_POOL[pk] == null){
+        var url,key;
+        if(tp == 'article'){
+            key = 'title';
+            url = parent.W3CNF.USERHOME+tp+"/"+pid+"/head.htm";
+        }else if(tp == 'gallery'){
+            key = 'brief';
+            url = parent.W3CNF.USERHOME+tp+"/info/"+pid+".htm";
+        }else{
+            return pid;
+        }
+        var txt = parent.A9Loader.syncLoadText(url);
+        var tmp = txt.split(/[\r\n]/);
+        for(var i=0; i<tmp.length; i++){
+            if(tmp[i].indexOf(key)>=0){
+                var pos = tmp[i].indexOf("=");
+                HEAD_POOL[pk] = pos>0?(tmp[i].substr(pos+1).replace(/^[ \t]+/g,"")):pid;
+                break;
+            }
+        }
+    }
+    return HEAD_POOL[pk];
+}
+
 function stopLoad(){
     isStop = true;
 }
+
 function listEvent(){
     try{
         dateFr.setFullYear(document.getElementById("__Y4_FM__").value);
@@ -26,8 +57,8 @@ function listEvent(){
         alert("bad [from:"+dateFr+"] or [to:"+dateTo+"]");
         return;
     }
-    
     isStop = false;
+    isHead = document.getElementById("__HEAD__").checked;
     document.getElementById("__SHOW__").disabled = !isStop;
     document.getElementById("__STOP__").disabled = isStop;
     document.getElementById("ACTION").innerHTML = "";
@@ -60,18 +91,18 @@ function loadData(){
                 if(parts.length != 4) continue;
                 tcnt++;
                 html.push("<tr height='"+lineHeight+"' style='background-color:"+(tcnt%5==0?splitbg:blankbg)+"'>");
-                html.push("<td>&nbsp;"+parts[0]+"</td>");
-                html.push("<td>&nbsp;"+parts[1]+"</td>");
-                html.push("<td>&nbsp;"+parts[2]+"</td>");
-                html.push("<td>&nbsp;"+parts[3]+"</td>");
+                html.push("<td>&nbsp;"+parts[0]+"</td>"); // type
+                html.push("<td>&nbsp;"+getHead(parts[0],parts[1])+"</td>"); // pid
+                html.push("<td>&nbsp;"+parts[2]+"</td>"); // time
+                html.push("<td>&nbsp;"+parts[3]+"</td>"); // from
                 html.push("</tr>");
             }
         }
         if(tcnt>0){
             html.push("</table>");
             var head = "<table border='0' cellspacing='1' cellpadding='2' width='100%'>"+
-                       "<tr height='"+lineHeight+"' style='background-color:"+datexbg+"'><td> + "+
-                       dstr+"("+tcnt+")</td><td>pid</td><td>time</td><td>from</td></tr>";
+                       "<tr height='"+lineHeight+"' style='background-color:"+datexbg+"'><td width='120px'> + "+
+                       dstr+"("+tcnt+")</td><td>pid</td><td width='100px'>time</td><td width='100px'>from</td></tr>";
             document.getElementById("ACTION").innerHTML+=head+html.join("");
         }
         
@@ -150,6 +181,8 @@ function initIndex(){
     html.push("<input type='button' id='__SHOW__' onclick='listEvent()' value='Show' style='font-size=12px; background-color:#99CCFF;'>");
     html.push("&nbsp;");
     html.push("<input type='button' id='__STOP__' onclick='stopLoad()' disabled value='Stop' style='font-size=12px; background-color:#FFCC99;'>");
+    html.push("&nbsp;");
+    html.push("<input type='checkbox' id='__HEAD__'><label for='__HEAD__'>Title(sync)</label>");
     document.getElementById("INDEX").innerHTML=html.join("");
 }
 //
