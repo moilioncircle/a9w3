6383696d90a2ccb26c4e8642cbb5a422848812d0
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2009-03-04 13:08:29 +0000

    *** empty log message ***

--- trunk/a9w3/a9w3-engine/core/common/start.js
+++ trunk/a9w3/a9w3-engine/core/common/start.js
@@ -7,11 +7,22 @@ W3GUI.asyncInit(function(){
     W3GUI.drawPage();
 }
 );
+function showA9W3Ad(){
+    var adobj = document.getElementById("__A9W3_AD__");
+    if(adobj.style.visibility !='visible'){
+        adobj.style.visibility='visible';
+        window.setTimeout(hideA9W3Ad,3000);
+    }
+}
+
+function hideA9W3Ad(){
+    document.getElementById("__A9W3_AD__").style.visibility='hidden';
+}
 
 // test speed
 if(W3CNF.SPEED_CHAR_BS>0 && W3CNF.SPEED_CHAR_BS<5000){ //<5k/s
     // low speed warn
-    var html = "<div id='__SPEED_WARN__' style=\"border-bottom:1px solid red;position:absolute;left:0px;top:0px;width:100%;height:20px;z-index:100;background-color:#efefef;text-align:center;color:#FF0000;font-size:12px;padding-top:4px\" onClick=\"this.style.visibility='hidden'\">";
+    var html = "<div id='__SPEED_WARN__' class='a9w3_warn_box' onClick=\"this.style.visibility='hidden'\">";
         html+= "(-_-!) low speed ("+W3CNF.SPEED_CHAR_BS+"b/s) ... ";
         html+= "</div>";
     document.write(html);
