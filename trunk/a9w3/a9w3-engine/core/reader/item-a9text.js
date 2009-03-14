e9b87e1c77d76cdee9da9d2278162f74b08ad402
trydofor <trydofor@c82cb51b-4197-49b0-975f-f48e9da5a4da>
2009-03-14 07:53:41 +0000

    encrypt all

--- trunk/a9w3/a9w3-engine/core/reader/item-a9text.js
+++ trunk/a9w3/a9w3-engine/core/reader/item-a9text.js
@@ -4,16 +4,45 @@ var root = A9Loader.getPagePath();
 var base = document.getElementsByTagName("BASE");
 if(base != null && base.length>0) root = base[0].href;
 
+var isEncrypted = false;
+var TEXT_CRYPT_REGEX = /(^\s*-----------------------+ a9-encrypted-text -----------------------+\s*)/;
+if(TEXT_CRYPT_REGEX.test(text)){
+    text = text.substr(RegExp.$1.length);
+    isEncrypted = true;
+}else{
+    text = text.replace(/^\s+/,'');
+}
+
 A9Loader.asyncLoadClass('com.trydofor.a9js.ui.pgbar');
 A9Loader.asyncLoadClass('com.trydofor.a9text.parser.a9text');
 A9Loader.asyncLoadClass('com.trydofor.a9text.render.html.a9text');
+if(isEncrypted){
+A9Loader.asyncLoadClass('com.trydofor.a9js.code.b64');
+A9Loader.asyncLoadClass('com.trydofor.a9js.code.aes');
+}
+
 A9Loader.runAfterClassLoaded(function(){
-try{
+
+if(isEncrypted){ // decrypt
+    try{
+        var passwd = window.prompt('please input your passwd','');
+        if(passwd ==null || passwd =='') return;
+        
+        var b64Impl  = new B64Class();
+        var aesImpl  = new AESClass();
+        text = aesImpl.decrypt(b64Impl.decode(text.replace(/\s/gm,'')),passwd,256);
+    }catch(e){
+        alert("failed to get base64 decode:"+e);
+        return;
+    }
+}
+
+try{ // parse a9text
     var pgbar = new ProgressBar(window.document);
     pgbar.show();
     pgbar.work(1,'parsing ...');
     var a9dom = new A9Dom(null,A9Dom.type.root); //com.trydofor.a9text.A9Dom
-    a9dom.setText(text.replace(/^[\r\n]+/,''));
+    a9dom.setText(text);
     a9dom.putInfo(A9Dom.type.root$path,root);
     var a9tParser = new A9TextParser();
     a9tParser.setProgressBar(pgbar);
