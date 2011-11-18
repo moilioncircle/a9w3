6cac5bbad1c832c747e1333df51ec4303292e4e5
Shi Rongjiu <trydofor@gmail.com>
2011-11-18 09:45:41 +0000

    sitectrl http download and manual

--- a9w3/a9w3-engine/util/sitectrl/sitemanager.php
+++ a9w3/a9w3-engine/util/sitectrl/sitemanager.php
@@ -18,7 +18,7 @@ function parseCommand($argv){
     $options = array();
     // parse arguments
     foreach($argv as $k=>$v){
-        if($k<2) continue; // skip 
+        if($k<2) continue; // skip
         if(strpos($v,'-') === 0){ // options
             $es = strpos($v,'=');
             if($es){
@@ -44,11 +44,14 @@ function parseCommand($argv){
         case 'ftpxfp':
             command_ftpxfp($cmndarg,$options);
             break;
+        case 'httpfp':
+            command_httpfp($cmndarg,$options);
+            break;
         case 'help':
         default:
             command_help();
     }
-} 
+}
 //////////////////////// Command ///////////////////////////
 function command_sitefp($cmndarg,$options){
     if(empty($cmndarg[0]) || !is_dir(PATH_ROOT.'a9w3-auhome/'.$cmndarg[0])){
@@ -56,18 +59,18 @@ function command_sitefp($cmndarg,$options){
         exit;
     }
     $treeDir = array(
-        PATH_ROOT.'index.htm',
-        PATH_ROOT.'a9w3-engine',
-        PATH_ROOT.'a9w3-server',
-        PATH_ROOT.'a9w3-auhome/'.$cmndarg[0]
+    PATH_ROOT.'index.htm',
+    PATH_ROOT.'a9w3-engine',
+    PATH_ROOT.'a9w3-server',
+    PATH_ROOT.'a9w3-auhome/'.$cmndarg[0]
     );
-    
+
     $siteFp = new SiteFp(PATH_ROOT);
     if(!empty($options['in'])){
-	    $siteFp->setInclude($options['in']);
+        $siteFp->setInclude($options['in']);
     }
     if(!empty($options['ex'])){
-	    $siteFp->setExclude($options['ex']);
+        $siteFp->setExclude($options['ex']);
     }
     foreach($treeDir as $v){
         echo $siteFp->makeFp($v);
@@ -83,29 +86,29 @@ function command_difffp($cmndarg,$options){
         exit;
     }
     if($cmndarg[0] == $cmndarg[1]) return;
-    
+
     $inFile = array_key_exists('in', $options)?explode(',',$options['in']):array();
     $exFile = array_key_exists('ex', $options)?explode(',',$options['ex']):array();
-    
+
     $lfplines = fpfile2array($cmndarg[0],$inFile,$exFile);
     $rfplines = fpfile2array($cmndarg[1],$inFile,$exFile);
-    
+
     //token | filename | lsize | ltime | rsize | rtime
     $tk_lcl = '>>';// >> : local  only / !md5 && ltime>=rtime
     $tk_rmt = '<<';// << : remote only / !md5 && ltime<rtime
     $tk_eql = '==';// == : equal (md5 && size)
     $tk_unk = '<>';// <> : unknown,none of above
     $tk_spl = '|';
-    
+
     //based on local
     $showEq = array_key_exists('eq', $options);
     foreach($lfplines as $fn=>$op){
         if(!array_key_exists($fn,$rfplines)){
             echo $tk_lcl.$tk_spl.       // token
-                 $fn.$tk_spl.           // filename
-                 $op['size'].$tk_spl.   // lsize
-                 $op['time'].$tk_spl.   // ltime
-                 $tk_spl.               // rsize
+            $fn.$tk_spl.           // filename
+            $op['size'].$tk_spl.   // lsize
+            $op['time'].$tk_spl.   // ltime
+            $tk_spl.               // rsize
                  "\n";                  // rtime
         }else{
             $rop = $rfplines[$fn];
@@ -119,23 +122,23 @@ function command_difffp($cmndarg,$options){
                 }
             }
             echo $tk_its.$tk_spl.       // token
-                 $fn.$tk_spl.           // filename
-                 $op['size'].$tk_spl.   // lsize
-                 $op['time'].$tk_spl.   // ltime
-                 $rop['size'].$tk_spl.  // rsize
-                 $rop['time']."\n";     // rtime
+            $fn.$tk_spl.           // filename
+            $op['size'].$tk_spl.   // lsize
+            $op['time'].$tk_spl.   // ltime
+            $rop['size'].$tk_spl.  // rsize
+            $rop['time']."\n";     // rtime
         }
     }
-    
+
     //remote only
     foreach($rfplines as $fn=>$op){
         if(!array_key_exists($fn,$lfplines)){
             echo $tk_rmt.$tk_spl.     // token
-                 $fn.$tk_spl.         // filename
-                     $tk_spl.         // lsize
-                     $tk_spl.         // ltime
-                 $op['size'].$tk_spl. // rsize
-                 $op['time']."\n";    // rtime
+            $fn.$tk_spl.         // filename
+            $tk_spl.         // lsize
+            $tk_spl.         // ltime
+            $op['size'].$tk_spl. // rsize
+            $op['time']."\n";    // rtime
         }
     }
 }
@@ -144,21 +147,21 @@ function command_ftpxfp($cmndarg,$options){
         echo 'bad parameter: difffp';
         exit;
     }
-    
+
     $fpfile = $cmndarg[0];
     if(!is_file($fpfile)){
         echo 'file not found: '.$fpfile;
         exit;
     }
-    
+
     $op_put = 'put';
     $op_get = 'get';
     $op_del = 'del';
     $op_nop = 'nop';
-    
+
     $lcl_opr = $op_put;
     $rmt_opr = $op_get;
-    
+
     if(array_key_exists('lcl', $options)){
         $op = $options['lcl'];
         if($op == $op_put
@@ -181,7 +184,7 @@ function command_ftpxfp($cmndarg,$options){
             exit;
         }
     }
-    
+
     $tk_put = '>>';// >> : upload/del
     $tk_get = '<<';// << : download/del
     $tk_spl = '|';
@@ -202,11 +205,11 @@ function command_ftpxfp($cmndarg,$options){
             echo 'skipe line:'.$line."\n";
         }
     }
-	if(empty($put_arr) && empty($get_arr)){
+    if(empty($put_arr) && empty($get_arr)){
         echo 'difffp list is empty,skip it';
         exit;
     }
-    
+
     $ftpstr = $cmndarg[1];//"user[:passwd]@server[:port] a9w3"
     $ps_at = strpos($ftpstr,'@');
     if($ps_at>0){
@@ -231,27 +234,27 @@ function command_ftpxfp($cmndarg,$options){
         $host=intval(substr($host,0,$ps_at));
     }
     if(empty($host)||empty($port)){
-    	echo 'bad parameter: ftp_server_string';
-    	exit;
+        echo 'bad parameter: ftp_server_string';
+        exit;
     }
     echo 'ftp host: '.$host.':'.$port."\n";
-      
+
     // open ftp
-	$conn_id = ftp_connect($host,$port);
-	if(!ftp_login($conn_id,$user,$pass)){
-		echo 'failed to login';
-		exit;
-	}
-	
-	// check home
-	echo 'current pwd: '.ftp_pwd($conn_id)."\n";
-	if(!empty($cmndarg[2])){
-		echo 'change to a9w3 home: '.$cmndarg[2]."\n";
-		if(!ftp_chdir($conn_id, $cmndarg[2])){
-			echo 'failed to change to a9w3home dir';
-			exit;
-		}
-	}
+    $conn_id = ftp_connect($host,$port);
+    if(!ftp_login($conn_id,$user,$pass)){
+        echo 'failed to login';
+        exit;
+    }
+
+    // check home
+    echo 'current pwd: '.ftp_pwd($conn_id)."\n";
+    if(!empty($cmndarg[2])){
+        echo 'change to a9w3 home: '.$cmndarg[2]."\n";
+        if(!ftp_chdir($conn_id, $cmndarg[2])){
+            echo 'failed to change to a9w3home dir';
+            exit;
+        }
+    }
     // do put
     foreach($put_arr as $k=>$v){
         $lclfile = PATH_ROOT.$k;
@@ -299,10 +302,74 @@ function command_ftpxfp($cmndarg,$options){
             echo ($ok?'OK ':"ERR").' RMT_GET '.$k."\n";
         }
     }
-	// close ftp
-	ftp_close($conn_id);
-    
+    // close ftp
+    ftp_close($conn_id);
+
 }
+
+function command_httpfp($cmndarg,$options){
+    if(count($cmndarg)<2){
+        echo 'bad parameter: difffp';
+        exit;
+    }
+
+    $fpfile = $cmndarg[0];
+    $server = $cmndarg[1];
+    if(!is_file($fpfile)){
+        echo 'file not found: '.$fpfile;
+        exit;
+    }
+
+    $tk_get = '<<';// << : download
+    $tk_spl = '|';
+    //loop in fp file
+    $get_arr = array();
+    foreach(file($fpfile) as $line){
+        $pts = explode($tk_spl,$line);
+        if(count($pts) != 6){
+            echo 'skipe line:'.$line."\n";
+            continue;
+        }
+        if($pts[0] === $tk_get){
+            $get_arr[]=$pts[1];
+        }
+    }
+    if(empty($get_arr)){
+        echo 'difffp list is empty,skip it';
+        exit;
+    }
+
+    // do get
+    foreach($get_arr as $v){
+        $lclfile = PATH_ROOT.$v;
+        //get
+        // check and make dir on local
+        if(!is_dir(dirname($lclfile))){
+            $tkpt = '/';
+            $crpt = PATH_ROOT;
+            foreach(explode($tkpt,dirname($v)) as $dn){
+                if(empty($dn)) continue;
+                $crpt.= $dn.$tkpt;
+                if(!is_dir($crpt)){
+                    mkdir($crpt);
+                }
+            }
+        }
+        $rmthand = fopen($server.'/'.$v, "rb");
+        $bstring = stream_get_contents($rmthand);
+        fclose($rmthand);
+        if($bstring){
+            $lclhand = fopen($lclfile, "wb");
+            if(fwrite($lclhand, $bstring) !== FALSE){
+                echo $v."  OK\n";
+            }
+            fclose($lclhand);
+            continue;
+        }
+        echo $v."  ERR\n";
+    }
+}
+
 function command_help(){
     echo "
     a simple a9w3 site manager to maintain site at local side.
@@ -338,6 +405,10 @@ function command_help(){
                            get: download every file from remote.
                            del: del remote file which not exist at local.
                            nop: skip all.
+        * httpfp difffp server
+            useing http to get file remote by difffp.
+            difffp      made by 'difffp'.
+            server      the a9w3 root url of the http server.
         * help
             what you see just now.
     ";
@@ -345,12 +416,12 @@ function command_help(){
 
 //////////////////////// Helper ///////////////////////////
 class SiteFp{
-    
+
     var $root = NULL;
     var $inar = NULL;
     var $exar = NULL;
     var $tkns = '|';
-    
+
     function SiteFp($root){
         $this->root = $root?trim($root):NULL;
     }
@@ -371,7 +442,7 @@ class SiteFp{
     }
     function makeFp($path){
         if(!file_exists($path)) return '';
-        
+
         $result = '';
         if (is_dir($path)){
             if ($dh = opendir($path)){
@@ -401,9 +472,9 @@ class SiteFp{
                 if($gt) return '';
             }
             $result.=str_replace($this->root,'',$path).$this->tkns.
-                filesize($path).$this->tkns.
-                date('YmdHis',filemtime($path)).$this->tkns.
-                md5_file($path)."\n";
+            filesize($path).$this->tkns.
+            date('YmdHis',filemtime($path)).$this->tkns.
+            md5_file($path)."\n";
         }
         return $result;
     }
@@ -411,7 +482,7 @@ class SiteFp{
 
 function fpfile2array($fn,$in,$ex){
     $fpa = array();
-    
+
     if($in){
         foreach($in as $k=>$v){
             if($v){
@@ -426,11 +497,11 @@ function fpfile2array($fn,$in,$ex){
             }
         }
     }
-    
+
     foreach(file($fn) as $ln){
         $pts = explode('|',trim($ln));
         if(count($pts) != 4) continue;
-        
+
         if($in){
             $gt = false;
             foreach($in as $p){
@@ -439,7 +510,7 @@ function fpfile2array($fn,$in,$ex){
             }
             if(!$gt) continue;
         }
-        
+
         if($ex){
             $gt = true;
             foreach($ex as $p){
@@ -448,7 +519,7 @@ function fpfile2array($fn,$in,$ex){
             }
             if($gt) continue;
         }
-        
+
         $fpa[$pts[0]] = array('size'=>$pts[1],'time'=>$pts[2],'md5'=>$pts[3]);
     }
     return $fpa;
